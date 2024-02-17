a18ad36f8f68b7a7a1e87d6e0608662498ceaffc
niknacevski <73705046+niknacevski@users.noreply.github.com>
2024-02-17 14:26:01 +0100

    Add Backend

---
+++ backend/AkademskaMislaBackend/src/main/java/com/example/akademskamislabackend/AkademskaMislaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.akademskamislabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkademskaMislaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkademskaMislaBackendApplication.class, args);
+    }
+
+}
---
+++ backend/AkademskaMislaBackend/src/test/java/com/example/akademskamislabackend/AkademskaMislaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akademskamislabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkademskaMislaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
